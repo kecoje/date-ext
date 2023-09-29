b7cdef76946113ccc92689f259a6cf1af3d80073
Милош <miloskecojevic907@gmail.com>
2023-09-29 16:28:52 +0200

    README & Exported config

---
+++ README.md
@@ -0,0 +1,10 @@
+# date-ext (Extended Date class)
+
+`date-ext` is a lightweight npm package that extends the native JavaScript Date class with additional utility methods for common date operations.
+
+## Installation
+
+You can install `date-ext` using npm:
+
+```bash
+npm install @kecoje/date-ext
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import DateExt from "./DateExt"
+import DateExt, { config } from "./DateExt"
 import Timezone from "./Timezone"
 import { monthMap, weekdayMap } from "./dateLabels";
 import { timezones } from "./timezones";
 
-export { DateExt, Timezone, monthMap, weekdayMap, timezones }
+export { DateExt, Timezone, monthMap, weekdayMap, timezones, config }
